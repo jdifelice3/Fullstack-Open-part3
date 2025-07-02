ed59c12bffceb023a36eb7e4e027ecfa959cd7f7
Your Name <johndifelice@protonmail.com>
2025-07-02 11:51:56 -0400

    Corrections made after running eslint.

--- index.js
+++ index.js
@@ -1,81 +1,81 @@
-const {errorHandler} = require('./errorHandlers/errorHandler');
+const {errorHandler} = require('./errorHandlers/errorHandler')
 
 const express = require('express')
-const morgan = require("morgan")
-require('dotenv').config();
-const Person = require('./models/Person'); 
-const connectDb = require('./db');
-const app = express(); 
-app.use(express.json());
-app.use(express.static('dist'));
+const morgan = require('morgan')
+require('dotenv').config()
+const Person = require('./models/Person') 
+const connectDb = require('./db')
+const app = express()
+app.use(express.json())
+app.use(express.static('dist'))
 
-connectDb(); 
+connectDb()
 
 morgan.token('body', (req) => {
-  return req.method === 'POST' ? JSON.stringify(req.body) : '';
-});
+    return req.method === 'POST' ? JSON.stringify(req.body) : ''
+})
 
 // Use morgan with custom format including the body
 app.use(morgan(':method :url :status :res[content-length] - :response-time ms :body', {
   
-}));
+}))
 
-app.use(errorHandler);
+app.use(errorHandler)
 
-app.get("/api/persons", async(req, res, next) => {
+app.get('/api/persons', async(req, res) => {
     try {
-        console.log('in api/persons');
-        console.log(typeof Person.getPersons); 
-        const persons = await Person.getPersons(); 
-        console.log('getPersons', persons);
-        res.send(persons);
+        console.log('in api/persons')
+        console.log(typeof Person.getPersons) 
+        const persons = await Person.getPersons() 
+        console.log('getPersons', persons)
+        res.send(persons)
     } catch (error) {
-        console.error('Error fetching persons:', error);
-        res.status(500).send('Internal Server Error');
+        console.error('Error fetching persons:', error)
+        res.status(500).send('Internal Server Error')
     }
 })
 
-app.get("/api/persons/:name", async(req, res, next) => {
-    const name = req.params.name;
-    console.log("name:",name);
-    const person = await Person.getPersons(name); 
+app.get('/api/persons/:name', async(req, res) => {
+    const name = req.params.name
+    console.log('name:',name)
+    const person = await Person.getPersons(name) 
     if(person){
-        res.send(person);
+        res.send(person)
     } else {
         res.status(404).send(`The person with name ${name} was not found.`)
     }
-    console.log("person:", person);
+    console.log('person:', person)
 })  
 
-app.get("/api/persons/id/:id", async(req, res, next) => {
-    const id = req.params.id;
-    console.log("name:",id);
-    const person = await Person.getPersonById(id); 
+app.get('/api/persons/id/:id', async(req, res) => {
+    const id = req.params.id
+    console.log('name:',id)
+    const person = await Person.getPersonById(id) 
     if(person){
-        res.send(person);
+        res.send(person)
     } else {
         res.status(404).send(`The person with name ${id} was not found.`)
     }
-    console.log("person:", person);
+    console.log('person:', person)
 })  
 
-app.delete("/api/persons/:id", async(req, res, next) => {
-    const id = req.params.id; 
-    console.log("id", id);
+app.delete('/api/persons/:id', async(req, res) => {
+    const id = req.params.id 
+    console.log('id', id)
     Person.deleteOne({ _id: id })
         .then((result) => {
-            console.log('Delete result:', result);
-            res.status(204).end();
+            console.log('Delete result:', result)
+            res.status(204).end()
         })
         .catch((err) => {
-            console.error(err);
-            res.status(500).send('Server error');
-        });
+            console.error(err)
+            res.status(500).send('Server error')
+        })
 })
 
-app.post("/api/persons", async(req, res, next) => {
-    const body = req.body;
-    console.log('body',body);
+app.post('/api/persons', async(req, res) => {
+    const body = req.body
+    console.log('body',body)
 
     if (!body) {
         return res.status(400).json({
@@ -93,30 +93,30 @@ app.post("/api/persons", async(req, res, next) => {
         })
     }
    
-    let person = new Person();
-    person.name = body.name;
-    person.number = body.number;
+    let person = new Person()
+    person.name = body.name
+    person.number = body.number
 
     return person.save().then((newPerson) => {
         res.json(newPerson)
     })
 })
 
-app.get("/info", (req ,res) => {
-    let dateTime = Date();
-    let html = `
+app.get('/info', (req ,res) => {
+    let dateTime = Date()
+    let html = `    
         <html>
             <body>
                 <div>Phone Book has info for 2 people</div>
                 <div>${dateTime}</div>
             </body>
-        </html>`;
+        </html>`
     
-        res.contentType = "html/text";
-        res.send(html);
+    res.contentType = 'html/text'
+    res.send(html)
 
 })
 
-const PORT = process.env.PORT || 3001;
+const PORT = process.env.PORT || 3001
 app.listen(PORT)
 console.log(`Server running on port ${PORT}`)
--- mongo.js
+++ mongo.js
@@ -1,37 +1,35 @@
 const mongoose = require('mongoose')
-const Person = require('./models/Person');
-require('dotenv').config();
-const name = process.argv[3];
-const number = process.argv[4];
-let id = "";
+const Person = require('./models/Person')
+require('dotenv').config()
+const name = process.argv[3]
+const number = process.argv[4]
+console.log('encoded pwd', process.argv[2])
 
-console.log("encoded pwd", process.argv[2]);
-
-const url = process.env.MONGODB_URI;
-console.log('url',url);
+const url = process.env.MONGODB_URI
+console.log('url',url)
 
 mongoose.set('strictQuery',false)
 
-mongoose.connect(url);
+mongoose.connect(url)
 
 if(process.argv.length <= 3){
     //return the list of persons in the db
     Person.find({}).then(result => {
         result.forEach(person => {
-        console.log(person)
+            console.log(person)
         })
-        mongoose.connection.close();
+        mongoose.connection.close()
     })
-    return;
+    return
 }
 
 const person = new Person({
-  id: Person.generateId(30),
-  name: name,
-  number: number,
+    id: Person.generateId(30),
+    name: name,
+    number: number,
 })
 
 person.save().then(result => {
-  console.log('person saved!')
-  mongoose.connection.close()
+    console.log('person saved!')
+    mongoose.connection.close()
 })
